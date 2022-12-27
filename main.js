222ed8f305b286a79fa64379f82415990df5b0bb
Juliano Peres <106198988+julianoaperes@users.noreply.github.com>
2022-12-27 19:15:09 -0300

    estrutura de repetição for
    estrutura de repetição for

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
-function tocaSomPom() {
-    document.querySelector('#som_tecla_pom').play()
+function tocaSom(idElementoAudio) {
+    document.querySelector(idElementoAudio).play()
 }
 
 const ListaDeTeclas = document.querySelectorAll('.tecla');
 
-ListaDeTeclas[0].onclick = tocaSomPom
+
+for (let contador = 0; contador < ListaDeTeclas.length; contador++) {
+
+    const tecla = ListaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    console.log(contador);
+}
